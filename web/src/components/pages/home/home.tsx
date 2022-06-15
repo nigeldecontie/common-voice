1beb900aa66f017197b7a3cae132ee7778dd83cb
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2022-06-15 10:28:34 -0400

    Change CV's logo for an ILT placeholder logo.

--- web/src/components/pages/home/home.tsx
+++ web/src/components/pages/home/home.tsx
@@ -171,7 +171,7 @@ export default function HomePage() {
           </div>
         </section>
       ) : (
-        <RegisterSection marsSrc="/img/mars.svg">
+        <RegisterSection marsSrc="/img/ChangeMe.png">
           <Localized id="help-make-dataset">
             <h1 />
           </Localized>
